327aba181a4682b983e7fbd71fc1945cc3f37913
kwame-Owusue <alfredowusu595@gmail.com>
2024-06-17 13:10:05 +0100

    removed read time

--- src/components/readingTime.js
+++
@@ -1,9 +0,0 @@
-function readingTime(text){
-    const wordsPerMinute = 200;
-    const noOfWords = text.split(/\s+/).length;
-    const minutes = noOfWords / wordsPerMinute;
-    const readTime = Math.ceil(minutes);
-    return `${readTime} minute read`;
-}
-
-export default readingTime;
