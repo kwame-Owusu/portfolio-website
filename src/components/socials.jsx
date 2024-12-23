eea1a37ad4f50549aee1049ad4b473c74ee13af2
Kwame <alfredowusu595@gmail.com>
2024-12-23 18:52:54 +0000

    implementing new look

--- src/components/socials.jsx
+++ src/components/socials.jsx
@@ -15,7 +15,6 @@ const Socials = () => {
 				<a href="https://www.linkedin.com/in/alfredow23/" target="_blank">
 					<svg  className="social-arrow" xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" fill="white">
 						<polygon points="7 7 15.586 7 5.293 17.293 6.707 18.707 17 8.414 17 17 19 17 19 5 7 5 7 7"/>
-					
 					</svg>
 						LinkedIn
 						
