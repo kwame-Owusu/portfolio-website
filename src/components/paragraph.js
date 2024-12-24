3afa984a4c9a19de69cb510cdfce09b713a291f8
Kwame <alfredowusu595@gmail.com>
2024-12-24 20:05:50 +0000

    finished, for the most part

--- src/components/paragraph.js
+++
@@ -1,9 +0,0 @@
-import styled from "@emotion/styled"
-
-const Paragraph  = styled.p`
-    text-align: justify;
-    margin-top: 15px;
-    text-indent: 1em;
-    hyphens: auto;
-`
-export default Paragraph
