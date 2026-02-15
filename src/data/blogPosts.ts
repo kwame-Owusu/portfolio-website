3ed8457b56086d00f2adc45163ab24166a44f827
kwame-Owusu <alfredowusu595@gmail.com>
2026-02-15 19:56:29 +0000

    refactor blog posts, use mapping over data we have

---
+++ src/data/blogPosts.ts
@@ -0,0 +1,21 @@
+export interface blogPost {
+    title: string;
+    date: string;
+    href: string;
+    img: string;
+}
+
+export const blogPosts: blogPost[] = [
+    {
+        href: 'posts/competence-as-a-virtue',
+        title: 'Competence as a virtue',
+        date: 'Jan 21st, 2026',
+        img: '/blog/infinite_vagabond.png'
+    },
+    {
+        href: 'posts/in-pursuit-of-depth',
+        title: 'In pursuit of depth',
+        date: 'Jan 3rd, 2025',
+        img: '/blog/astro.jpg'
+    }
+]
