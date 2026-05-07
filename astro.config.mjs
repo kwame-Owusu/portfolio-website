bfc354db951a6abf8109f21570aeacccf4001903
kwame-Owusu <alfredowusu595@gmail.com>
2026-05-07 14:20:28 +0100

    revert packages back

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
-import vercel from "@astrojs/vercel";
+import vercel from "@astrojs/vercel/serverless";
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [react(), mdx()],
-  adapter: vercel(),
 });
