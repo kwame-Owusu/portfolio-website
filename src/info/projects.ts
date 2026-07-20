5382a77a957b84f3e3895f3c445e6ac5a8d9da78
kwame-Owusu <alfredowusu595@gmail.com>
2026-07-20 12:31:59 +0100

    chore: rename folder to info and replace accorcia project with geode

--- src/info/projects.ts
+++ src/info/projects.ts
@@ -6,23 +6,23 @@ export interface Project {
 
 export const projects: Project[] = [
     {
-        projectName: "lista",
+        projectName: "Lista",
         projectDesc: "Your todo + notes manager on the terminal",
         projectLink: "https://github.com/kwame-Owusu/lista",
     },
     {
-        projectName: "sintesi",
-        projectDesc: "Simple system info fetch utility",
+        projectName: "Geode",
+        projectDesc: "Remote sync, MCP, and an API for your Obsidian vault.",
+        projectLink: "https://github.com/8thpark/geode",
+    },
+    {
+        projectName: "Sintesi",
+        projectDesc: "System info fetch utility",
         projectLink: "https://github.com/kwame-Owusu/sintesi",
     },
     {
-        projectName: "sidl",
+        projectName: "Sidl",
         projectDesc: "A cli tool to get information about twilio sids",
         projectLink: "https://github.com/kwame-Owusu/sidl",
-    },
-    {
-        projectName: "accorcia",
-        projectDesc: "URL shortener service with redis as data store",
-        projectLink: "https://github.com/kwame-Owusu/accorcia",
     },
 ];
