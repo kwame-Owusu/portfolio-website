c0e531853c0d1498e61c6af32864b78bc2c5487b
kwame-Owusu <alfredowusu595@gmail.com>
2026-07-30 15:52:33 +0100

    refactor: change the styling for work section

--- src/info/experiences.ts
+++ src/info/experiences.ts
@@ -1,29 +1,24 @@
 export interface Experience {
   expLink: string;
   expLogo: string;
-  expTitle: string;
-  expDesc: string;
-  expIconClass?: string;
-  backgroundColor?: string;
-  hoverColor?: string;
+  expCompany: string;
+  expRole: string;
+  expYear: string;
 }
 
 export const experiences: Experience[] = [
   {
-    expTitle: 'Zing',
-    expDesc: 'Full Stack',
+    expCompany: 'Zing',
+    expRole: 'Full Stack',
+    expYear: '2026',
     expLink: 'https://zing.dev/',
     expLogo: './zing-logo.svg',
-    backgroundColor: 'rgba(129, 82, 247, 0.3)',
-    hoverColor: 'rgba(129, 82, 247, 0.67)',
   },
   {
-    expTitle: 'Creo',
-    expDesc: 'Web & Data',
+    expCompany: 'Creo',
+    expRole: 'Web & Data',
+    expYear: '2025',
     expLink: 'https://www.creoofficial.com/',
     expLogo: './creo-logo.jpg',
-    expIconClass: 'custom-icon-class',
-    backgroundColor: 'rgba(0, 0, 0, 0.2)',
-    hoverColor: 'rgba(0, 0, 0, 0.25)',
   },
 ];
